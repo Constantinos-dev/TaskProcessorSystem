f58a6edcab50ca7c56706f685fcbb25d13b7a88a
Constantinos Xenofontos <cxenofontos2@gmail.com>
2025-04-07 00:56:24 +0300

    Commit

--- TaskProcessorSystem/Models/Jobs.cs
+++ TaskProcessorSystem/Models/Jobs.cs
@@ -2,7 +2,17 @@
 
 namespace TaskProcessorSystem.Models
 {
-    public class Jobs
+    public enum JobStatus
     {
+        Pending,
+        Running,
+        Completed,
+        Failed
+    }
+
+    public class Job
+    {
+        public Guid Id { get; set; } = Guid.NewGuid(); // Unique identifier
+        public string Type { get; set; } // Type of job
     }
 }
