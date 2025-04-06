009f612b0c2350adc66ef1e4889e746af3f028af
Constantinos Xenofontos <cxenofontos2@gmail.com>
2025-04-07 01:13:31 +0300

    Commit

---
+++ TaskProcessorSystem/Data/JobDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskProcessorSystem.Models;
+
+namespace TaskProcessorSystem.Data
+{
+    public class JobDbContext : DbContext
+    {
+        public JobDbContext(DbContextOptions<JobDbContext> options) : base(options) { }
+
+        public DbSet<Job> Jobs { get; set; }  // EF will create a Jobs table
+    }
+}
