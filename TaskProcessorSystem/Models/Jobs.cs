db966c106ac00c1ca29cd0d1b2c78a5e5712179d
Constantinos Xenofontos <cxenofontos2@gmail.com>
2025-04-07 01:09:33 +0300

    Commit All and Push

--- TaskProcessorSystem/Models/Jobs.cs
+++ TaskProcessorSystem/Models/Jobs.cs
@@ -14,5 +14,11 @@ namespace TaskProcessorSystem.Models
     {
         public Guid Id { get; set; } = Guid.NewGuid(); // Unique identifier
         public string Type { get; set; } // Type of job
+        public string PayloadJson { get; set; }          // Task data in JSON
+        public JobStatus Status { get; set; } = JobStatus.Pending;
+        public int RetryCount { get; set; } = 0;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? CompletedAt { get; set; }
+        public string? ErrorMessage { get; set; }
     }
 }
