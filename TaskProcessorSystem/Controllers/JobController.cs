5786d3ed0608f9b190aaee9379b3d64440c16a2e
Constantinos Xenofontos <cxenofontos2@gmail.com>
2025-04-07 12:37:06 +0300

    Update with error handling

--- TaskProcessorSystem/Controllers/JobController.cs
+++ TaskProcessorSystem/Controllers/JobController.cs
@@ -2,6 +2,7 @@
 using TaskProcessorSystem.Models;
 using TaskProcessorSystem.Data;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace TaskProcessorSystem.Controllers
 {
@@ -19,23 +20,55 @@ namespace TaskProcessorSystem.Controllers
         [HttpPost]
         public async Task<ActionResult<Job>> CreateJob(Job job)
         {
-            _context.Jobs.Add(job);
-            await _context.SaveChangesAsync();
-            return CreatedAtAction(nameof(GetJobById), new { id = job.Id }, job);
+            try
+            {
+                _context.Jobs.Add(job);
+                await _context.SaveChangesAsync();
+                return CreatedAtAction(nameof(GetJobById), new { id = job.Id }, job);
+            }
+            catch (DbUpdateException dbEx)
+            {
+                
+                return StatusCode(500, "A database error occurred while creating the job.");
+            }
+            catch (Exception ex)
+            {
+                
+                return StatusCode(500, "An unexpected error occurred while creating the job.");
+            }
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Job>>> GetJobs()
         {
-            return await _context.Jobs.ToListAsync();
+            try
+            {
+                var jobs = await _context.Jobs.ToListAsync();
+                return Ok(jobs);
+            }
+            catch (Exception ex)
+            {
+                
+                return StatusCode(500, "An error occurred while retrieving jobs.");
+            }
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Job>> GetJobById(Guid id)
         {
-            var job = await _context.Jobs.FindAsync(id);
-            if (job == null) return NotFound();
-            return job;
+            try
+            {
+                var job = await _context.Jobs.FindAsync(id);
+                if (job == null)
+                    return NotFound();
+
+                return Ok(job);
+            }
+            catch (Exception ex)
+            {
+                
+                return StatusCode(500, "An error occurred while retrieving the job.");
+            }
         }
     }
 }
