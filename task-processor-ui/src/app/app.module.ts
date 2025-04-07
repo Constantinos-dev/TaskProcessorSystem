3e9f3ddc5f93fde97b6b1322a1d0f8a1f7482fff
Constantinos Xenofontos <cxenofontos2@gmail.com>
2025-04-08 00:39:59 +0300

    declare components, fix

--- task-processor-ui/src/app/app.module.ts
+++ task-processor-ui/src/app/app.module.ts
@@ -1,11 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { JobListComponent } from './job-list/job-list.component';
+import { JobSubmitComponent } from './job-submit/job-submit.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [...],
-  imports: [
-    ...,
-    HttpClientModule
+  declarations: [
+    AppComponent,
+    JobListComponent,
+    JobSubmitComponent
   ],
-  ...
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
